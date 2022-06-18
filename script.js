89e199c298ce09edb4f6dbb50cb7d742313f3869
Luiz <lhvalentim@outlook.coom>
2022-06-18 09:57:54 -0300

    refac ui

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-var url = 'square'
-var dotsStyle = 'extra-rounded'
+var url = ''
+var dotsStyle = 'rounded'
+var cornerStyle = 'extra-rounded'
 var colorPrimary = '#000'
 var colorSecondary = '#000'
 var colorAccent = '#000'
+var colorBg = '#fff'
 var logo = ''
+var width = Math.min(512, (document.body.clientWidth - 24))
+var height = Math.min(512, (document.body.clientWidth - 24))
+
+const reader = new FileReader()
+reader.onload = e => {
+	logo = e.target.result
+	refreshQRCode()
+}
 
 const qrCode = new QRCodeStyling({
-	width: Math.min(512, document.body.clientWidth),
-	height: Math.min(512, document.body.clientWidth),
 	errorCorrectionLevel: 'H',
 	imageOptions: {
 		crossOrigin: 'anonymous',
 		imageSize: 0.5,
 		margin: 10
-	},
+	}
 })
-qrCode.append(document.querySelector('main'))
 
 function refreshQRCode() {
 	qrCode.update({
+		width: width,
+		height: height,
 		data: url,
 		image: logo,
 		dotsOptions: {
@@ -30,45 +39,58 @@ function refreshQRCode() {
 		},
 		cornersSquareOptions: {
 			color: colorAccent,
-			type: dotsStyle
-		}
+			type: cornerStyle
+		},
+		backgroundOptions: {
+			color: colorBg,
+		},
 	})
 }
 
-const reader = new FileReader()
-reader.onload = e => {
-	logo = e.target.result
-	refreshQRCode()
+function init() {
+	let main = document.querySelector('main')
+	main.style.setProperty('width', `${width}px`)
+	main.style.setProperty('height', `${height}px`)
+	qrCode.append(main)
+	document.querySelector('#url').onchange = e => {
+		if (!e.target.value) return
+		url = e.target.value
+		refreshQRCode()
+	}
+	document.querySelector('#color-primary').onchange = e => {
+		if (!e.target.value) return
+		colorPrimary = e.target.value
+		refreshQRCode()
+	}
+	document.querySelector('#color-secondary').onchange = e => {
+		if (!e.target.value) return
+		colorSecondary = e.target.value
+		refreshQRCode()
+	}
+	document.querySelector('#color-accent').onchange = e => {
+		if (!e.target.value) return
+		colorAccent = e.target.value
+		refreshQRCode()
+	}
+	document.querySelector('#color-bg').onchange = e => {
+		if (!e.target.value) return
+		colorBg = e.target.value
+		refreshQRCode()
+	}
+	document.querySelector('#logo').onchange = e => {
+		let picture = e.target.files[0]
+		if (picture) reader.readAsDataURL(picture)
+	}
+	document.querySelector('#download').onclick = () => {
+		try {
+			qrCode.download({name: 'qrcode', extension: 'png'})
+		} catch(e) {
+			alert(e)
+		}
+	}
 }
 
-document.querySelector('#url').onchange = e => {
-	if (!e.target.value) return
-	url = e.target.value
-	refreshQRCode()
-}
-document.querySelector('#color-primary').onchange = e => {
-	if (!e.target.value) return
-	colorPrimary = e.target.value
-	refreshQRCode()
-}
-document.querySelector('#color-secondary').onchange = e => {
-	if (!e.target.value) return
-	colorSecondary = e.target.value
-	refreshQRCode()
-}
-document.querySelector('#color-accent').onchange = e => {
-	if (!e.target.value) return
-	colorAccent = e.target.value
-	refreshQRCode()
-}
-document.querySelector('#logo').onchange = e => {
-	let picture = e.target.files[0]
-	if (picture) reader.readAsDataURL(picture)
-}
-document.querySelector('#download').onclick = () => {
-	try {
-		qrCode.download({name: 'qrcode', extension: 'png'})
-	} catch(e) {
-		alert(e)
-	}
+document.onreadystatechange = () => {
+	if (document.readyState != 'complete') return
+	init()
 }
